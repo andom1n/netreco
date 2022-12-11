94ebc1a0801489348dd73aa5121031b22673f1f7
Andrei Tarasov <adnominator@bk.ru>
2022-12-11 22:39:16 +0500

    add

---
+++ src/test/java/MaxServiceTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+public class MaxServiceTest {
+
+    @Test
+    public void shouldFindMax() {
+        MaxService service = new MaxService();
+
+        int a = 5;
+        int b = 3;
+
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
